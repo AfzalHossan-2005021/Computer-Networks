e7c17b0c495fe0d4b5f57807456dfd659f707950
Afzal Hossan <afzalhossan2005021@gmail.com>
2024-09-10 18:04:13 +0600

    Identification of NOT FOUND url added.

--- Socket-Programing/src/Server/ReadWriteThread.java
+++ Socket-Programing/src/Server/ReadWriteThread.java
@@ -14,7 +14,34 @@ public class ReadWriteThread implements Runnable{
         this.client = client;
         new Thread(this).start();
     }
-
+    
+    String handleTxtFile(String relativePath){
+        String body = "";
+        return  body;
+    }
+    String handleImageFile(String relativePath){
+        String body = "";
+        return  body;
+    }
+    String handleOtherFile(String relativePath){
+        String body = "";
+        return  body;
+    }
+    String generateFileHtml(String relativePath){
+        String body = "";
+        if(relativePath.contains(".txt")){
+            body += handleTxtFile(relativePath);
+        } else if (relativePath.contains(".png") || relativePath.contains(".jpg")) {
+            body += handleImageFile(relativePath);
+        } else {
+            body += handleOtherFile(relativePath);
+        }
+        StringBuilder html = new StringBuilder();
+        html.append("<!DOCTYPE html>\n<html>\n<head>\n<title></title>\n</head>\n<body>\n");
+        html.append(body);
+        html.append("\n</body>\n</html>");
+        return html.toString();
+    }
     String generateDirectoryHtml(String[] directoryContents, String parent){
         StringBuilder html = new StringBuilder();
         html.append("<!DOCTYPE html>\n<html>\n<head>\n<title></title>\n</head>\n<body>\n");
@@ -30,10 +57,16 @@ public class ReadWriteThread implements Runnable{
             }
             html.append("</a><br>");
         }
-        html.append("</body>\n</html>");
+        html.append("\n</body>\n</html>");
         return html.toString();
     }
-
+    String generateErrorMessage(){
+        StringBuilder html = new StringBuilder();
+        html.append("<!DOCTYPE html>\n<html>\n<head>\n<title></title>\n</head>\n<body>\n");
+        html.append("<h6>404: Page not found</h6>");
+        html.append("\n</body>\n</html>");
+        return html.toString();
+    }
     @Override
     public void run(){
         try {
@@ -44,13 +77,20 @@ public class ReadWriteThread implements Runnable{
                 if(partsOfRequest[0].equals("GET")){
                     String relativePath = ROOT + partsOfRequest[1].replace("%20", " ");
                     File path = new File(relativePath);
-                    String[] contents = path.list();
-                    if (contents != null) {
-                        String parent = "http://localhost:" + PORT + partsOfRequest[1];
-                        if(!partsOfRequest[1].endsWith("/")){
-                            parent += "/";
+                    if(path.exists()){
+                        String htmlContent = "";
+                        if(path.isDirectory()){
+                            String[] contents = path.list();
+                            if (contents != null) {
+                                String parent = "http://localhost:" + PORT + partsOfRequest[1];
+                                if(!partsOfRequest[1].endsWith("/")){
+                                    parent += "/";
+                                }
+                                htmlContent += generateDirectoryHtml(contents, parent);
+                            }
+                        } else if (path.isFile()) {
+                            htmlContent += generateFileHtml(relativePath);
                         }
-                        String htmlContent = generateDirectoryHtml(contents, parent);
                         socketWrapper.write("HTTP/1.1 200 OK\r\n");
                         socketWrapper.write("Server: Java HTTP Server: 1.0\r\n");
                         socketWrapper.write("Date: " + new Date() + "\r\n");
@@ -59,6 +99,17 @@ public class ReadWriteThread implements Runnable{
                         socketWrapper.write("\r\n");
                         socketWrapper.write(htmlContent);
                         socketWrapper.flush();
+                    } else {
+                        String errorMessage = generateErrorMessage();
+                        socketWrapper.write("HTTP/1.1 r 404 NOT FOUND\r\n");
+                        socketWrapper.write("Server: Java HTTP Server: 1.0\r\n");
+                        socketWrapper.write("Date: " + new Date() + "\r\n");
+                        socketWrapper.write("Content-Type: html\r\n");
+                        socketWrapper.write("Content-Length: " + errorMessage.length() + "\r\n");
+                        socketWrapper.write("\r\n");
+                        socketWrapper.write(errorMessage);
+                        socketWrapper.flush();
+                        System.out.println("404: Page not found");
                     }
                 }
             }
